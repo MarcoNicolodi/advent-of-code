6eef5588dc2d25ab37cac77deb70cf26b1533a2b
Marco Nicolodi <marcocontatopro@gmail.com>
2020-12-16 22:45:24 -0300

    day 8 part 2

--- src/advent_of_code_2020/day8.clj
+++ src/advent_of_code_2020/day8.clj
@@ -1,6 +1,6 @@
 (ns advent-of-code-2020.day8
-  (:require  [clojure.string :as str]
-             [clojure.set    :as set]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
 (def path "./resources/day8-input")
 
@@ -12,18 +12,18 @@
 (defn accumulate [state value]
   (-> state
       (update :accumulated (partial + value))
-      (update :ran-ops     #(conj % (:position state)))
-      (update :position    inc)))
+      (update :ran-ops #(conj % (:position state)))
+      (update :position inc)))
 
 (defn noop [state _]
   (-> state
-      (update :ran-ops     #(conj % (:position state)))
-      (update :position    inc)))
+      (update :ran-ops #(conj % (:position state)))
+      (update :position inc)))
 
 (defn jump [state value]
   (-> state
-      (update :ran-ops     #(conj % (:position state)))
-      (update :position    (partial + value))))
+      (update :ran-ops #(conj % (:position state)))
+      (update :position (partial + value))))
 
 (def op->fn
   {:accumulate accumulate
@@ -53,3 +53,57 @@
         (recur ((op->fn op) state value))))))
 
 (def part1 (comp run-ops wire->op+value))
+
+(defn infinite-loop? [state]
+  ((:ran-ops state) (:position state)))
+
+(defn last-op? [state ops+values]
+  (let [[_ _ last?] (nth ops+values (:position state))]
+    last?))
+
+(def op->switch-op
+  {:jump :noop
+   :noop :jump})
+
+(defn flag-last-op [ops+values]
+  (-> ops+values
+      vec
+      (subvec 0 (dec (count ops+values)))
+      (conj (conj (last ops+values) :last-op))))
+
+(defn next-switchable-op-idx [ops+values]
+  (reduce (fn [idx [op & _]]
+            (if (op->switch-op op)
+              (reduced idx)
+              (inc idx)))
+          0
+          ops+values))
+
+(defn switch-next-switchable-op [ops+values {idx :last-switched-op-idx :as state}]
+  (if idx
+    (let [before-switch (subvec ops+values 0 idx)
+          last-switched-op (nth ops+values idx)
+          after-switch (vec (drop (inc idx) ops+values))
+          undo-last-switch (update last-switched-op 0 op->switch-op)
+          next-switchable-index (next-switchable-op-idx after-switch)
+          switched (update after-switch next-switchable-index (fn [op+value] (update op+value 0 op->switch-op)))
+          new-ops+values (concat (conj before-switch undo-last-switch) switched)
+          new-state (assoc (new-state) :last-switched-op-idx (+ (:last-switched-op-idx state) (inc next-switchable-index)))]
+      [new-ops+values new-state])
+    (let [next-switchable-index (next-switchable-op-idx ops+values)
+          new-ops+values (update ops+values next-switchable-index (fn [op+value] (update op+value 0 op->switch-op)))
+          new-state (assoc (new-state) :last-switched-op-idx next-switchable-index)]
+      [new-ops+values new-state])))
+
+(defn run-ops-with-failure-switching [ops+values]
+  (loop [ops+values (flag-last-op ops+values)
+         state (new-state)]
+    (if (infinite-loop? state)
+      (let [[switched-ops+values state] (switch-next-switchable-op (vec ops+values) state)]
+        (recur switched-ops+values state))
+      (if (last-op? state ops+values)
+        (:accumulated state)
+        (let [[op value] (nth ops+values (:position state))]
+         (recur ops+values ((op->fn op) state value)))))))
+
+(def part2 (comp run-ops-with-failure-switching wire->op+value))
