85c487d65ad47a3666db7e3eed225e4c7983d7e9
Marco Nicolodi <marcocontatopro@gmail.com>
2023-01-15 21:41:11 -0300

    day 6

--- src/advent_of_code/2022/day6.clj
+++ src/advent_of_code/2022/day6.clj
@@ -1,16 +1,16 @@
 (ns advent-of-code.2022.day6)
 
 
-(defn part-1 []
+(defn runner [window-size]
   (let [input-stream
         (clojure.java.io/input-stream
           (clojure.java.io/file "./resources/2022/day6-input"))]
-    (loop [window [(.read input-stream)
-                   (.read input-stream)
-                   (.read input-stream)
-                   (.read input-stream)]
-           cursor-position 4]
-      (if (= 4 (count (into #{} window)))
+    (loop [window (vec (repeatedly window-size (fn [] (.read input-stream))))
+           cursor-position window-size]
+      (if (= window-size (count (into #{} window)))
         cursor-position
         (recur (conj (vec (drop 1 window)) (.read input-stream))
                (inc cursor-position))))))
+
+(def part-1 (runner 4))
+(def part-1 (runner 14))
